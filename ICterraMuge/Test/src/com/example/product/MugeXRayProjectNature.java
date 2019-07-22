cd11bcc68854abcca7417a70014b0ad6cc48d6ba
Öner ELMAS <oelmas@gmail.com>
2019-07-22 14:04:01 +0300

    Test
    Test class start. For project open related test.

---
+++ ICterraMuge/Test/src/com/example/product/MugeXRayProjectNature.java
@@ -0,0 +1,36 @@
+package com.example.product;
+
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectNature;
+
+import org.eclipse.core.runtime.CoreException;
+
+public class MugeXRayProjectNature implements IProjectNature {
+
+	public static final String NATURE_ID="mugeXRayNature";
+	
+	@Override
+	public void configure() throws CoreException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deconfigure() throws CoreException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public IProject getProject() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setProject(IProject project) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
